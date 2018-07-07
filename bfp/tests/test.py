f26a50dfada1ab8df319f9807e90b2aae08c6bbe
amar2012 <amarenderreddy012@gmail.com>
2018-07-07 22:19:21 +0530

    Add initial unit test file.

---
+++ bfp/tests/test.py
@@ -0,0 +1,27 @@
+import unittest
+from bfp import app
+
+class ProjectTests(unittest.TestCase):
+    
+    def setUp(self):
+        app.config['DEBUG'] = False
+        app.config['TEST'] = True
+        self.app = app.test_client()
+
+        self.assertEquals(app.config['DEBUG'], False)
+    
+    def tearDown(self):
+        pass
+
+    def test_home_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'1', response.data)
+        self.assertIn(b'New List Item', response.data)
+        self.assertIn(b'This is a basic app built with Flask', response.data)
+        self.assertIn(b'2', response.data)
+        self.assertIn(b'3', response.data)
+        self.assertIn(b'4', response.data)
+        self.assertIn(b'5', response.data)
+    
+if __name__ == '__main__':
+    unittest.main() 
